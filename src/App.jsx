f4436297e3be695e01e6ea9f719225a008e02df3
Sakshi149 <chavan06sakshi@gmail.com>
2025-02-14 15:05:48 +0530

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Tasks from './tasks'
 import Header from './Header'
+import Register from './Register'
+import Login from './login'
+import Logout from './logout'
+import MyInfo from './MyInfo'
 
 const App = () => {
   return (
@@ -9,7 +13,12 @@ const App = () => {
       <BrowserRouter>
       <Header/>
         <Routes>
-          <Route path='' element={<Tasks/>}></Route>
+          <Route path='/' element={<Login/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+          <Route path='/register' element={<Register/>}></Route>
+          <Route path='/tasks' element={<Tasks/>}></Route>
+          <Route path='/logout' element={<Logout/>}></Route>
+          <Route path='/myinfo' element={<MyInfo/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
